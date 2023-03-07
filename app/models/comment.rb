72f054f6307d044a875fa4c2417d42b16df26ad4
SandeepPyata <pyatasandeepsandy@gmail.com>
2023-03-07 11:42:33 +0530

    Annotate models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  comment     :text
+#  user_id     :integer          not null
+#  question_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :question
--- test/models/answer_vote_test.rb
+++ test/models/answer_vote_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: answer_votes
+#
+#  id         :integer          not null, primary key
+#  upvote     :integer
+#  downvote   :integer
+#  user_id    :integer          not null
+#  answer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class AnswerVoteTest < ActiveSupport::TestCase
