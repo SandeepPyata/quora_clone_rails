8e8da412f7cf9ccacf176245d010ec5f6bd1977e
SandeepPyata <pyatasandeepsandy@gmail.com>
2023-03-24 00:10:56 +0530

    Modified answer votes logic

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,23 +3,26 @@ class Answer < ApplicationRecord
   belongs_to :question
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
+  has_many :answer_votes
 
-  def self.answer_upvote(answer,current_user)
+  def self.answer_upvote(answer, current_user)
     # Check if user voted to question by fetching record (if exists)
-    user_voted = AnswerVote.find_by(answer_id: answer.id, user_id: current_user.id)
+    user_voted =
+      AnswerVote.find_by(answer_id: answer.id, user_id: current_user.id)
     message = ""
     if user_voted
       if user_voted.upvote?
         message = "Answer already upvoted"
       else
-        user_voted.upvote = 1
-        user_voted.downvote = 0      # as user already downvoted & user wants to upvote; so, change downvote to 0
+        user_voted.upvote += 1
+        user_voted.downvote -= 1 # as user already downvoted & user wants to upvote; so, change downvote to 0
         user_voted.save
         message = "Previously downvoted; Now upvoted"
       end
     else
-      answer_vote = AnswerVote.new(user_id: current_user.id, answer_id: answer.id)
-      answer_vote.upvote = 1
+      answer_vote =
+        AnswerVote.new(user_id: current_user.id, answer_id: answer.id)
+      answer_vote.upvote += 1
       if answer_vote.save
         message = "Answer upvoted"
       else
@@ -29,22 +32,24 @@ class Answer < ApplicationRecord
     return message
   end
 
-  def self.answer_downvote(answer,current_user)
+  def self.answer_downvote(answer, current_user)
     # Check if user voted to answer by fetching record (if exists)
-    user_voted = AnswerVote.find_by(answer_id: answer.id, user_id: current_user.id)
+    user_voted =
+      AnswerVote.find_by(answer_id: answer.id, user_id: current_user.id)
     message = ""
     if user_voted
       if user_voted.downvote?
         message = "Answer already downvoted"
       else
-        user_voted.downvote = 1
-        user_voted.upvote = 0     # as user already upvoted & user wants to downvote; so, change upvote to 0
+        user_voted.downvote += 1
+        user_voted.upvote -= 1 # as user already upvoted & user wants to downvote; so, change upvote to 0
         user_voted.save
         message = "Previously upvoted; Now downvoted"
       end
     else
-      answer_vote = AnswerVote.new(user_id: current_user.id, answer_id: answer.id)
-      answer_vote.downvote = 1
+      answer_vote =
+        AnswerVote.new(user_id: current_user.id, answer_id: answer.id)
+      answer_vote.downvote += 1
       if answer_vote.save
         message = "Answer downvoted"
       else
@@ -53,5 +58,4 @@ class Answer < ApplicationRecord
     end
     return message
   end
-
 end
